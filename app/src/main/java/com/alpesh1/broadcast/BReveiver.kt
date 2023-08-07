43a30cd7103f5da197abc805d661593f82b2a5bb
Alpesh Rathava <alpeshrathava301@gmail.com>
2023-08-07 21:24:26 +0530

    Commit_Ready_Broadcast

--- app/src/main/java/com/alpesh1/broadcast/BReveiver.kt
+++ app/src/main/java/com/alpesh1/broadcast/BReveiver.kt
@@ -7,35 +7,37 @@ import android.content.Intent
 import android.telephony.TelephonyManager
 import android.util.Log
 
-class BReveiver: BroadcastReceiver() {
+class BReveiver : BroadcastReceiver() {
 
-    private val TAG = "BReveiver"
+    private val TAG = "PhoneStatReceiver"
+
     private var incomingFlag = false
+
     private var incoming_number: String? = null
 
-    override fun onReceive(p0: Context?, intent: Intent?) {
+    override fun onReceive(p0: Context?, p1: Intent?) {
 
-
-        if (intent!!.action.equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
+        if (p1?.getAction().equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
             incomingFlag = false
-            val phoneNumber = intent!!.getStringExtra(Intent.EXTRA_PHONE_NUMBER)
-            Log.i(TAG, "call OUT:$phoneNumber")
+            val phoneNumber: String? = p1?.getStringExtra(Intent.EXTRA_PHONE_NUMBER)
+            Log.e(TAG, "call OUT:$phoneNumber")
         } else {
 
+            val tm = p0?.getSystemService(Service.TELEPHONY_SERVICE) as TelephonyManager
 
-            when (p0?.getSystemService(Service.TELEPHONY_SERVICE)?.javaClass) {
-                TelephonyManager.CALL_STATE_RINGING.javaClass -> {
+            when (tm.callState) {
+                TelephonyManager.CALL_STATE_RINGING -> {
                     incomingFlag = true
-                    incoming_number = intent!!.getStringExtra("incoming_number")
-                    Log.i(TAG, "RINGING :$incoming_number")
+                    incoming_number = p1?.getStringExtra("incoming_number")
+                    Log.e(TAG, "RINGING :$incoming_number")
                 }
 
-                TelephonyManager.CALL_STATE_OFFHOOK.javaClass -> if (incomingFlag) {
-                    Log.i(TAG, "incoming ACCEPT :$incoming_number")
+                TelephonyManager.CALL_STATE_OFFHOOK -> if (incomingFlag) {
+                    Log.e(TAG, "incoming ACCEPT :$incoming_number")
                 }
 
-                TelephonyManager.CALL_STATE_IDLE.javaClass -> if (incomingFlag) {
-                    Log.i(TAG, "incoming IDLE")
+                TelephonyManager.CALL_STATE_IDLE -> if (incomingFlag) {
+                    Log.e(TAG, "incoming IDLE")
                 }
             }
         }
